2833c582294c79a8fff01ac7f7b57fd6914aacbc
Sudeepha <your-email@example.com>
2026-03-11 19:23:44 +0530

    connect frontend with render backend

--- src/api/petApi.js
+++ src/api/petApi.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/pets';
-
+const API_BASE_URL = 'https://backend-pet-edhd.onrender.com/api/pets';
 const petApi = {
   getAllPets: async () => {
     try {
